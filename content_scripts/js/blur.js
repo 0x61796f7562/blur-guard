926abebff5b14798e507594acee82a34a492bfa8
ayoub <ayoubmansouriemail@gmail.com>
2025-01-11 22:42:20 +0100

    save manually blured & ublured elements

--- content_scripts/js/blur.js
+++ content_scripts/js/blur.js
@@ -13,7 +13,31 @@ function canIncludeDistraction(node) {
 	return false;
 }
 
-function setBlurFilter(rootNode) {
+async function isBlured(element) {
+	if (!element) return false;
+	const hostname = window.location.hostname;
+	const options = await browser.storage.local.get([hostname]);
+	const hostnameOptionsValue = options[hostname];
+	if (!hostnameOptionsValue) return false;
+	if (!hostnameOptionsValue.blured_elements) return false;
+	return hostnameOptionsValue.blured_elements.some((selector) =>
+		Boolean(element.closest(selector)),
+	);
+}
+
+async function isUnblured(element) {
+	if (!element) return false;
+	const hostname = window.location.hostname;
+	const options = await browser.storage.local.get([hostname]);
+	const hostnameOptionsValue = options[hostname];
+	if (!hostnameOptionsValue) return false;
+	if (!hostnameOptionsValue.unblured_elements) return false;
+	return hostnameOptionsValue.unblured_elements.some((selector) =>
+		Boolean(element.closest(selector)),
+	);
+}
+
+function setBlurFilter(rootNode, skipSavedElements = false) {
 	if (
 		rootNode.nodeType == Node.ELEMENT_NODE &&
 		canIncludeDistraction(rootNode) &&
@@ -30,21 +54,28 @@ function setBlurFilter(rootNode) {
 
 	while (treeWalker.nextNode()) {
 		const node = treeWalker.currentNode;
-		if (canIncludeDistraction(node) && node.tagName != "BODY") {
-			node.style.setProperty("filter", filterValue, "important");
-			intersectionObserver.observe(node);
-		}
-		if (node.openOrClosedShadowRoot && node.tagName != "VIDEO") {
-			mutationObserver.observe(
-				node.openOrClosedShadowRoot,
-				mutationObserverOptions,
-			);
-			setBlurFilter(node.openOrClosedShadowRoot);
-		}
+		isUnblured(skipSavedElements ? node : null).then((skip) => {
+			if (!skip) {
+				if (canIncludeDistraction(node) && node.tagName != "BODY") {
+					node.style.setProperty("filter", filterValue, "important");
+					intersectionObserver.observe(node);
+				}
+				if (node.openOrClosedShadowRoot && node.tagName != "VIDEO") {
+					mutationObserver.observe(
+						node.openOrClosedShadowRoot,
+						mutationObserverOptions,
+					);
+					setBlurFilter(
+						node.openOrClosedShadowRoot,
+						skipSavedElements,
+					);
+				}
+			}
+		});
 	}
 }
 
-function unsetBlurFilter(rootNode) {
+function unsetBlurFilter(rootNode, skipSavedElements = false) {
 	if (
 		rootNode.nodeType == Node.ELEMENT_NODE &&
 		canIncludeDistraction(rootNode) &&
@@ -61,20 +92,27 @@ function unsetBlurFilter(rootNode) {
 
 	while (treeWalker.nextNode()) {
 		const node = treeWalker.currentNode;
-		if (canIncludeDistraction(node) && node.tagName != "BODY") {
-			node.style.removeProperty("filter");
-			intersectionObserver.unobserve(node);
-		}
-		if (node.openOrClosedShadowRoot && node.tagName != "VIDEO") {
-			unsetBlurFilter(node.openOrClosedShadowRoot);
-		}
+		isBlured(skipSavedElements ? node : null).then((skip) => {
+			if (!skip) {
+				if (canIncludeDistraction(node) && node.tagName != "BODY") {
+					node.style.removeProperty("filter");
+					intersectionObserver.unobserve(node);
+				}
+				if (node.openOrClosedShadowRoot && node.tagName != "VIDEO") {
+					unsetBlurFilter(
+						node.openOrClosedShadowRoot,
+						skipSavedElements,
+					);
+				}
+			}
+		});
 	}
 }
 
 const intersectionObserver = new IntersectionObserver(
 	(entries) => {
 		entries.forEach((entry) => {
-			if (entry.isIntersecting) setBlurFilter(entry.target);
+			if (entry.isIntersecting) setBlurFilter(entry.target, true);
 		});
 	},
 	{ threshold: 0 },
@@ -92,14 +130,14 @@ const mutationObserver = new MutationObserver((mutations) => {
 			if (type == "childList") {
 				if (addedNodes) {
 					addedNodes.forEach((node) => {
-						setBlurFilter(node);
+						setBlurFilter(node, true);
 					});
 				}
 			} else if (type == "attributes") {
 				if (attributeName == "style") {
 					if (canIncludeDistraction(target)) {
 						if (getComputedStyle(target).filter != filterValue) {
-							setBlurFilter(target);
+							setBlurFilter(target, true);
 						}
 					}
 				}
@@ -131,13 +169,13 @@ function applyPageBlur() {
 	if (document.readyState == "loading") {
 		document.addEventListener("DOMContentLoaded", () => {
 			setTimeout(() => {
-				setBlurFilter(document.body);
+				setBlurFilter(document.body, true);
 				bodyBlurStyle.remove();
 				startObserver();
 			}, firstPageLoadDelay);
 		});
 	} else {
-		setBlurFilter(document.body);
+		setBlurFilter(document.body, true);
 		bodyBlurStyle.remove();
 		startObserver();
 	}
@@ -145,7 +183,7 @@ function applyPageBlur() {
 
 function removePageBlur() {
 	stopObserver();
-	unsetBlurFilter(document.body);
+	unsetBlurFilter(document.body, true);
 }
 
 const selectionBox = document.createElement("div");
@@ -213,6 +251,92 @@ function startElementSelection() {
 	};
 }
 
+function getCSSSelector(element) {
+	const path = [];
+	while (element) {
+		if (element === document.documentElement) {
+			path.unshift(element.tagName.toLowerCase());
+			break;
+		}
+
+		if (element.getRootNode() instanceof ShadowRoot) {
+			const shadowHost = element.getRootNode().host;
+			path.unshift(`:host ${element.tagName.toLowerCase()}`);
+			element = shadowHost;
+		} else {
+			let selector = element.tagName.toLowerCase();
+			if (element.id) {
+				selector = `#${element.id}`;
+				path.unshift(selector);
+				break;
+			} else {
+				let sibling = element;
+				let nth = 1;
+				while (sibling.previousElementSibling) {
+					sibling = sibling.previousElementSibling;
+					if (sibling.nodeName.toLowerCase() === selector) nth++;
+				}
+				if (nth !== 1) selector += `:nth-of-type(${nth})`;
+				path.unshift(selector);
+			}
+			element = element.parentElement;
+		}
+	}
+	return path.join(" > ");
+}
+
+function saveBluredElement(element) {
+	const hostname = window.location.hostname;
+	browser.storage.local.get([hostname]).then((options) => {
+		let hostnameOptionsValue = options[hostname] || {};
+		hostnameOptionsValue = {
+			...hostnameOptionsValue,
+			blured_elements: [
+				...new Set([
+					...(hostnameOptionsValue.blured_elements
+						? hostnameOptionsValue.blured_elements
+						: []),
+					getCSSSelector(element),
+				]),
+			],
+			unblured_elements: hostnameOptionsValue.unblured_elements
+				? hostnameOptionsValue.unblured_elements.filter(
+						(el) => getCSSSelector(element) != el,
+					)
+				: [],
+		};
+		browser.storage.local.set({
+			[hostname]: hostnameOptionsValue,
+		});
+	});
+}
+
+function saveUnbluredElement(element) {
+	const hostname = window.location.hostname;
+	browser.storage.local.get([hostname]).then((options) => {
+		let hostnameOptionsValue = options[hostname] || {};
+		hostnameOptionsValue = {
+			...hostnameOptionsValue,
+			unblured_elements: [
+				...new Set([
+					...(hostnameOptionsValue.unblured_elements
+						? hostnameOptionsValue.unblured_elements
+						: []),
+					getCSSSelector(element),
+				]),
+			],
+			blured_elements: hostnameOptionsValue.blured_elements
+				? hostnameOptionsValue.blured_elements.filter(
+						(el) => getCSSSelector(element) != el,
+					)
+				: [],
+		};
+		browser.storage.local.set({
+			[hostname]: hostnameOptionsValue,
+		});
+	});
+}
+
 (() => {
 	if (window.blur_guard_extension_loaded) return;
 	window.blur_guard_extension_loaded = true;
@@ -272,12 +396,14 @@ function startElementSelection() {
 						(e) => {
 							e.preventDefault();
 							e.stopImmediatePropagation();
+							const targetedElement = e.composed
+								? e.composedPath()[0]
+								: e.target;
 							unobserve(() => {
-								setBlurFilter(
-									e.composed ? e.composedPath()[0] : e.target,
-								);
+								setBlurFilter(targetedElement);
 								stopElementSelection();
 							});
+							saveBluredElement(targetedElement);
 						},
 						{ once: true, capture: true },
 					);
@@ -288,12 +414,14 @@ function startElementSelection() {
 						(e) => {
 							e.preventDefault();
 							e.stopImmediatePropagation();
+							const targetedElement = e.composed
+								? e.composedPath()[0]
+								: e.target;
 							unobserve(() => {
-								unsetBlurFilter(
-									e.composed ? e.composedPath()[0] : e.target,
-								);
+								unsetBlurFilter(targetedElement);
 								stopElementSelection();
 							});
+							saveUnbluredElement(targetedElement);
 						},
 						{ once: true, capture: true },
 					);
